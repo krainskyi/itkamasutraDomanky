36a9e123857c22e85953b477a16f7273f3f355a6
krainskyi <androidlap@gmail.com>
2021-09-06 13:52:38 +0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {Route,BrowserRouter} from "react-router-dom";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+
+                <div className='app-wrapper-content'>
+                    {/*<Route path='/profile' component={Profile}/>*/}
+                    {/*<Route path='/dialogs' component={Dialogs}/>*/}
+                    {/*<Route path='/message'/>*/}
+                    {/*<Route path='/music' />*/}
+                    {/*<Route path='/setting' />*/}
+
+
+                    {/*<Profile/>*/}
+                    {/*<Dialogs/>*/}
+                </div>
+            </div>
+        </BrowserRouter>
+
+    );
+}
+
+
+export default App;
+
